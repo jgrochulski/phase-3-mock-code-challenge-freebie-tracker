219cab312c5f0abad822f334d9143555f9fda1b6
jan <janek.grochulski@gmail.com>
2021-12-22 12:28:18 -0500

    fin

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,24 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
+
+  def recieved_one?(item_name)
+    if self.freebies.filter { |item| item.item_name == item_name }.length != 0
+      true
+    else
+      false
+    end
+  end
+
+  def give_away(dev, freebie)
+    if freebie.dev_id == self.id
+      puts "we gave #{freebie.item_name} to #{dev.name}"
+      dev.freebies << freebie
+      freebie.dev = dev
+    else
+      "not mine"
+    end
+  end
+
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,10 @@ Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
-
+Freebie.create(item_name: "Hoodie", value: 45, dev: Dev.find_by(name: "Rick"), company: Company.find_by(name: "Google"))
+Freebie.create(item_name: "Baseball Cap", value: 20, dev: Dev.find_by(name: "Morty"), company: Company.find_by(name: "Facebook"))
+Freebie.create(item_name: "T-Shirt", value: 25, dev: Dev.find_by(name: "Mr. Meseeks"), company: Company.find_by(name: "Dunder Mifflin"))
+Freebie.create(item_name: "Keychain", value: 5, dev: Dev.find_by(name: "Gazorpazop"), company: Company.find_by(name: "Enron"))
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
